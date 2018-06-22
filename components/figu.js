a7e14a714191c775180dd520d3e70a287585d129
florlafuente <lafuente.florencia@gmail.com>
2018-06-22 15:45:11 -0300

    Add figu component

---
+++ components/figu.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default () => (
+  <div className='figu-container'>
+    <div className='figu-header'>
+      <span>E</span>
+      <h2>Serbia</h2>
+    </div>
+    <div className='figu-body'>
+      <img src='' alt='Bandera' />
+      <table>
+        <tr>
+          <td>Data:</td>
+          <td>data</td> 
+        </tr>
+        <tr>
+          <td>Data:</td>
+          <td>data</td> 
+        </tr>
+        <tr>
+          <td>Data:</td>
+          <td>data</td> 
+        </tr>
+        <tr>
+          <td>Data:</td>
+          <td>data</td> 
+        </tr>
+        <tr>
+          <td>Data:</td>
+          <td>data</td> 
+        </tr>
+        <tr>
+          <td>Data:</td>
+          <td>data</td> 
+        </tr>
+      </table>
+    </div>
+    <div className='figu-footer'>
+      <img src='' alt='Chequeado' />
+      <img src='' alt='Mundial de la Igualdad' />
+      <img src='' alt='Las de Sistemas' />
+    </div>
+  </div>
+)
