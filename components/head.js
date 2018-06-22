eb9fe81352f005a709c3ffe34855941c916fba5e
florlafuente <lafuente.florencia@gmail.com>
2018-06-22 15:11:28 -0300

    Basic file structure

---
+++ components/head.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+
+export default () => (
+  <Head>
+    <title>Figus de la igualdad</title>
+    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    <link rel='stylesheet' type='text/css' href='static/global.css' />
+  </Head>
+)
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Head from '../components/head'
+
+export default () => (
+  <div>
+    <Head />
+    Hola mundo!
+  </div>
+)
