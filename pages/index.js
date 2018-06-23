b4ff8af0e8f28be886b11da2971df91220ea0c02
Nayla Portas <naylaportas@gmail.com>
2018-06-23 18:37:47 -0300

    Se agrega selector de paises

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
 import Head from '../components/head'
 import Figu from '../components/figu'
+import Dropdown from 'react-dropdown'
 
-export default () => (
-  <div>
-    <Head />
-    <Figu id="0"/>
-  </div>
-)
+export default class extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      pais: {value: 0, label: "Argentina"}
+    }
+    this.seleccionarPais = this.seleccionarPais.bind(this)
+  }
+
+  seleccionarPais(option) {
+    this.setState({pais: option})
+  }
+  
+  render() {
+    const paises =
+      [
+        {value: 0, label: 'Argentina'},
+        {value: 1, label: 'Nigeria'}
+      ]
+    const paisDefault = this.state.pais 
+
+    return (
+      <div>
+        <Head />
+        <Dropdown options={paises}
+          placeholder="Elegí un país"
+          onChange={this.seleccionarPais}
+          value={paisDefault}
+        />
+          
+        <Figu id={this.state.pais.value}/>
+      </div>
+      )
+  }
+}
