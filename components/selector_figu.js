cee1bbc7e0ea3f2485685a858c1366ddc811becd
Nayla Portas <naylaportas@gmail.com>
2018-06-23 19:19:32 -0300

    Completa los datos iniciales de los países

--- components/selector_figu.js
+++ components/selector_figu.js
@@ -20,8 +20,38 @@ export default class extends React.Component {
   render() {
     const paises =
       [
-        {value: 0, label: 'Argentina'},
-        {value: 1, label: 'Nigeria'}
+        {value:0, label:'Rusia'},
+        {value:1, label:'Arabia Saudí'},
+        {value:2, label:'Egipto'},
+        {value:3, label:'Uruguay'},
+        {value:4, label:'Marruecos'},
+        {value:5, label:'Irán'},
+        {value:6, label:'España'},
+        {value:7, label:'Portugal'},
+        {value:8, label:'Perú'},
+        {value:9, label:'Australia'},
+        {value:10, label:'Dinamarca'},
+        {value:11, label:'Francia'},
+        {value:12, label:'Croacia'},
+        {value:13, label:'Nigeria'},
+        {value:14, label:'Argentina'},
+        {value:15, label:'Islandia'},
+        {value:16, label:'Suiza'},
+        {value:17, label:'Serbia'},
+        {value:18, label:'Costa Rica'},
+        {value:19, label:'Brasil'},
+        {value:20, label:'México'},
+        {value:21, label:'Alemania'},
+        {value:22, label:'Corea del Sur'},
+        {value:23, label:'Suecia'},
+        {value:24, label:'Panamá'},
+        {value:25, label:'Túnez'},
+        {value:26, label:'Bélgica'},
+        {value:27, label:'Inglaterra'},
+        {value:28, label:'Japón'},
+        {value:29, label:'Polonia'},
+        {value:30, label:'Senegal'},
+        {value:31, label:'Colombia'},
       ]
     const paisDefault = this.state.pais 
 
