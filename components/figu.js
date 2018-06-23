0fac41aedd7b4a56cd7588a414b253a7cd7f090f
Nayla Portas <naylaportas@gmail.com>
2018-06-23 17:25:37 -0300

    Muestra info de Argentina

--- components/figu.js
+++ components/figu.js
@@ -1,38 +1,39 @@
 import React from 'react'
+import data_paises from '../static/assets/data_paises.json'
 
 
 export default () => (
   <div className='figu-container'>
     <div className='figu-header'>
-      <span>E</span>
-      <h2>Serbia</h2>
+      <span>{data_paises[0].grupo}</span>
+      <h2>{data_paises[0].nombre}</h2>
     </div>
     <div className='figu-body'>
       <img src='' alt='Bandera' />
       <table>
         <tr>
-          <td>Data:</td>
-          <td>data</td> 
+          <td>Esperanza de vida femenina:</td>
+          <td>{data_paises[0].esperanza_vida_mujer}</td> 
         </tr>
         <tr>
-          <td>Data:</td>
-          <td>data</td> 
+          <td>Año de voto femenino:</td>
+          <td>{data_paises[0].voto_femenino}</td> 
         </tr>
         <tr>
-          <td>Data:</td>
-          <td>data</td> 
+          <td>Porcentaje de legisladoras:</td>
+          <td>{data_paises[0].legisladoras}</td> 
         </tr>
         <tr>
-          <td>Data:</td>
-          <td>data</td> 
+          <td>Aborto:</td>
+          <td>{data_paises[0].aborto}</td> 
         </tr>
         <tr>
-          <td>Data:</td>
-          <td>data</td> 
+          <td>Días de licencia por maternidad/paternidad:</td>
+          <td>{data_paises[0].licencia_mat_pat}</td> 
         </tr>
         <tr>
-          <td>Data:</td>
-          <td>data</td> 
+          <td>Matrimonio igualitario:</td>
+          <td>{data_paises[0].matrimonio_igualitario}</td> 
         </tr>
       </table>
     </div>
