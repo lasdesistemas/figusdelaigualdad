5d7bfcdec4be2f9f5b7a59aad053625daa06ade9
florlafuente <lafuente.florencia@gmail.com>
2018-06-26 15:24:24 -0300

    Mejora estilos dropdown y agrega versus entre figuritas. Closes #15

--- components/head.js
+++ components/head.js
@@ -6,5 +6,6 @@ export default () => (
     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     <link rel='stylesheet' type='text/css' href='static/global.css' />
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet" />
   </Head>
 )
